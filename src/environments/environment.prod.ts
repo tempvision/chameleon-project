f72f0a744594b490ea6ccb574852594ad49b4cca
nydn <soodker@gmail.com>
2023-03-24 22:16:06 +0200

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCQ-B4QQumdwrsI_h3IkVjmhwENY8CJBHY",
+    authDomain: "chameleon-board-game.firebaseapp.com",
+    databaseURL: "https://chameleon-board-game-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "chameleon-board-game",
+    storageBucket: "chameleon-board-game.appspot.com",
+    messagingSenderId: "663808251011",
+    appId: "1:663808251011:web:62a691d370b2937594f7b6",
+    measurementId: "G-EZ8ES5XX4Z"
+  }
 };
